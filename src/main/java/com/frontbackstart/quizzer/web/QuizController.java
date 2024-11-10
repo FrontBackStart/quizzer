b6d303decd12ea61ec35fb9ea7b93708ed0e6ad2
Renne <renne.jamsen@gmail.com>
2024-11-10 12:55:52 +0200

    Make: quiz edit method. Renamed /addquiz -> /savequiz

--- src/main/java/com/frontbackstart/quizzer/web/QuizController.java
+++ src/main/java/com/frontbackstart/quizzer/web/QuizController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 //import java.util.List;
 @Controller
@@ -32,10 +33,15 @@ public class QuizController{
 		model.addAttribute("quiz", new Quiz());
 		return "addquiz";
 	}
-	@PostMapping("/addquiz")
+	@PostMapping("/savequiz")
 	public String saveQuiz(Quiz quiz){
 		quiz.setCreated(LocalDateTime.now());
 		quizRepository.save(quiz);
 		return "redirect:/quizzes";
+	}
+	@GetMapping("/editquiz/{quizId}")
+	public String editQuiz(@PathVariable("quizId") Integer quizId, Model model){
+		model.addAttribute("quiz", quizRepository.findById(quizId).orElseThrow());
+		return "editquiz";
 	}
 }
