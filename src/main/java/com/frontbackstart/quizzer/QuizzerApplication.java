876da7c84b6225d880570b45f792c3491926a506
Renne <renne.jamsen@gmail.com>
2024-11-17 16:36:18 +0200

    Fix: h2 testDataInitializer, don't create if not empty

--- src/main/java/com/frontbackstart/quizzer/QuizzerApplication.java
+++ src/main/java/com/frontbackstart/quizzer/QuizzerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.frontbackstart.quizzer.repository.AnswerRepository;
 import com.frontbackstart.quizzer.domain.Answer;
@@ -25,43 +26,48 @@ public class QuizzerApplication {
 	}
 
 	@Bean
-		public CommandLineRunner bookDemo(AnswerRepository answerRepo, QuestionRepository questionRepo, QuizRepository quizRepo) {
+		public CommandLineRunner testDataInitializer(AnswerRepository answerRepo, QuestionRepository questionRepo, QuizRepository quizRepo) {
 			LocalDateTime added = LocalDateTime.of(2021, 11, 07, 15, 15);
 			return (args) -> {
-				log.info("Create Quizzes");
-				Quiz quiz1 = new Quiz("Capital cities", "Quiz about capital cities", true, added);
-				quizRepo.save(quiz1);
-				LocalDateTime quiz2Added = LocalDateTime.of(2022, 11, 9, 9, 30);
-				Quiz quiz2 = new Quiz("Famous countries", "Quiz about countries", true, quiz2Added);
-				quizRepo.save(quiz2);
-				LocalDateTime quiz3Added = LocalDateTime.of(2023, 11, 9, 9, 30);  
-				Quiz quiz3 = new Quiz("Planets", "Quiz about planets", true, quiz3Added);
-				quizRepo.save(quiz3);
+				List<Quiz> quizzes = quizRepo.findAll();
+				if (quizzes.isEmpty()){
+					log.info("QuizRepo empty");
 
-				log.info("Create Questions");
-				Question question1 = new Question(quiz1, "What is the capital of Finland?", "Easy");
-				questionRepo.save(question1);
-				Question question2 = new Question(quiz1, "What is the capital of Sweden?", "Easy");
-				questionRepo.save(question2);
+					log.info("Create Quizzes");
+					Quiz quiz1 = new Quiz("Capital cities", "Quiz about capital cities", true, added);
+					quizRepo.save(quiz1);
+					LocalDateTime quiz2Added = LocalDateTime.of(2022, 11, 9, 9, 30);
+					Quiz quiz2 = new Quiz("Famous countries", "Quiz about countries", true, quiz2Added);
+					quizRepo.save(quiz2);
+					LocalDateTime quiz3Added = LocalDateTime.of(2023, 11, 9, 9, 30);
+					Quiz quiz3 = new Quiz("Planets", "Quiz about planets", true, quiz3Added);
+					quizRepo.save(quiz3);
 
-				log.info("Create Answers");
-				Answer ansA = new Answer(question1, "New York", false);
-				answerRepo.save(ansA);
-				Answer ansB = new Answer(question1, "Helsinki", true);
-				answerRepo.save(ansB);
-				Answer ansC = new Answer(question1, "Stockholm", false);
-				answerRepo.save(ansC);
-				Answer ansD = new Answer(question1, "Timbuktu", false);
-				answerRepo.save(ansD);
+					log.info("Create Questions");
+					Question question1 = new Question(quiz1, "What is the capital of Finland?", "Easy");
+					questionRepo.save(question1);
+					Question question2 = new Question(quiz1, "What is the capital of Sweden?", "Easy");
+					questionRepo.save(question2);
 
-				Answer ansE = new Answer(question2, "New York", false);
-				answerRepo.save(ansE);
-				Answer ansF = new Answer(question2, "Helsinki", false);
-				answerRepo.save(ansF);
-				Answer ansG = new Answer(question2, "Stockholm", true);
-				answerRepo.save(ansG);
-				Answer ansH = new Answer(question2, "Timbuktu", false);
-				answerRepo.save(ansH);
+					log.info("Create Answers");
+					Answer ansA = new Answer(question1, "New York", false);
+					answerRepo.save(ansA);
+					Answer ansB = new Answer(question1, "Helsinki", true);
+					answerRepo.save(ansB);
+					Answer ansC = new Answer(question1, "Stockholm", false);
+					answerRepo.save(ansC);
+					Answer ansD = new Answer(question1, "Timbuktu", false);
+					answerRepo.save(ansD);
+
+					Answer ansE = new Answer(question2, "New York", false);
+					answerRepo.save(ansE);
+					Answer ansF = new Answer(question2, "Helsinki", false);
+					answerRepo.save(ansF);
+					Answer ansG = new Answer(question2, "Stockholm", true);
+					answerRepo.save(ansG);
+					Answer ansH = new Answer(question2, "Timbuktu", false);
+					answerRepo.save(ansH);
+				};
 			};
 		}
 }
